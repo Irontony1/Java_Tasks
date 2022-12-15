21f8b8c2352584306db3843e49b04dc7ca024a5c
antondubov <antondubov1@yandex.ru>
2022-12-16 01:17:04 +0300

    floodFill

--- HW2/src/Main.java
+++ HW2/src/Main.java
@@ -1,4 +1,7 @@
 import Custom.TreeNode;
+
+import java.util.Arrays;
+
 public class Main {
     public static void main(String[] args) {
         // Вызов задачи 1
@@ -15,8 +18,12 @@ public class Main {
                                {0,0,0,0,0,0,0,1,1,1,0,0,0},
                                {0,0,0,0,0,0,0,1,1,0,0,0,0}};
         int out_task2 = maxAreaOfIsland(input_task2);
-        System.out.print(out_task2);
+        System.out.println(out_task2);
 
+        // Вызов задачи 3
+        int[][] input_task3 = {{1,1,1},{1,1,0},{1,0,1}};
+        int[][] out_task3 = floodFill(input_task3,1,1,2);
+        System.out.println(Arrays.deepToString(out_task3).replace("], ", "]\n"));
 
     }
     // Задача 1
@@ -48,6 +55,26 @@ public class Main {
 
         grid[i][j] = 0;
         return 1 + dfs(i - 1, j, grid) + dfs(i + 1, j, grid) + dfs(i, j - 1, grid) + dfs(i, j + 1, grid);
+    }
+
+    // Задача 3
+    public static int[][] floodFill(int[][] image, int sr, int sc, int color) {
+        if(image[sr][sc] != color) {
+            dfs(image, sr, sc, color, image[sr][sc]);
+        }
+        return image;
+    }
+
+    private static void dfs(int[][] image, int i, int j, int color, int cell) {
+        if (i < 0 || j < 0 || i >= image.length || j >= image[0].length || image[i][j] != cell) {
+            return;
+        }
+
+        image[i][j] = color;
+        dfs(image, i - 1, j, color, cell);
+        dfs(image, i + 1, j, color, cell);
+        dfs(image, i, j - 1, color, cell);
+        dfs(image, i, j + 1, color, cell);
     }
 }
 
