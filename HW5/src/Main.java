e7d03a30743befd41c4c3b92ac9ca78d8e346501
antondubov <antondubov1@yandex.ru>
2022-12-23 18:23:25 +0300

    findDuplicate

--- HW5/src/Main.java
+++ HW5/src/Main.java
@@ -9,5 +9,8 @@ public class Main {
         String[] words2 = {"amazing", "leetcode", "is"};
         System.out.println(Task1.countWords(words1, words2));
 
+        /* Задача 2 */
+        String[] paths = {"root/a 1.txt(abcd) 2.txt(efgh)","root/c 3.txt(abcd)","root/c/d 4.txt(efgh)","root 4.txt(efgh)"};
+        System.out.println(Task2.findDuplicate(paths));
     }
 }
