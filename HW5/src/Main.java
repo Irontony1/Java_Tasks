d82dd850fb7b6ab03e61f265f5da6a23b91a5bed
antondubov <antondubov1@yandex.ru>
2022-12-23 17:36:45 +0300

    countWords

---
+++ HW5/src/Main.java
@@ -0,0 +1,13 @@
+import java.util.HashMap;
+import java.util.Map;
+
+public class Main {
+    public static void main(String[] args) {
+
+        /* Задача 1 */
+        String[] words1 = {"leetcode", "is", "amazing", "as", "is"};
+        String[] words2 = {"amazing", "leetcode", "is"};
+        System.out.println(Task1.countWords(words1, words2));
+
+    }
+}
