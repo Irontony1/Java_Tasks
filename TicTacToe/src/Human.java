63fed88123de25ce7326bdb0ada52bad60ef1db9
antondubov <antondubov1@yandex.ru>
2024-03-19 14:38:33 +0300

    TicTacToe with UI

---
+++ TicTacToe/src/Human.java
@@ -0,0 +1,15 @@
+import java.util.concurrent.TimeUnit;
+
+public class Human {
+    private final char DOT;
+    public Human(char ch){
+        DOT = ch;
+    }
+
+    public void turn(int x, int y, Field field, AI ai) {
+          if(field.isCellValid(x,y)){
+              if (!field.isGameOver()) field.setDot(x,y,DOT);
+              if (!field.isGameOver()) ai.turn(field);
+          }
+    }
+}
